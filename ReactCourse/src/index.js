0d78023780b59b45f654512553c3b9a7e23004fa
John Wo <labmansoftac@gmail.com>
2020-07-09 11:00:38 +0300

    Basic structure including store added

--- ReactCourse/src/index.js
+++ ReactCourse/src/index.js
@@ -1,11 +1,29 @@
 import './main.css'
-import {createStore} from 'redux';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
 import { createBrowserHistory } from 'history';
+import thunk from 'redux-thunk';
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from 'react-redux';
 
-const history = createBrowserHistory( )
+import createRootReducer from 'reducers'; 
+import Layout from 'containers/layout';
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]; 
 const store = createStore(
-  createRootReducer(history)
-
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
 );
 
-document.write('Hello React/Redux!')
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Layout/>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
