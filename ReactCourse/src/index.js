3447a53191707bc9ebc72425a05e3ad2d28b0cf6
John Wo <labmansoftac@gmail.com>
2020-07-09 10:35:29 +0300

    React course, internet store

---
+++ ReactCourse/src/index.js
@@ -0,0 +1,11 @@
+import './main.css'
+import {createStore} from 'redux';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory( )
+const store = createStore(
+  createRootReducer(history)
+
+);
+
+document.write('Hello React/Redux!')
