49b865cfa9bc37df30605004204fa31878eec12d
LtsProfessional <labmansoftac@gmail.com>
2020-08-19 01:21:13 +0300

    topBar menu added

--- CoursesReact/medium_clone/src/index.js
+++ CoursesReact/medium_clone/src/index.js
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'routes'
+import TopBar from 'components/topBar'
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to Hooks</h3>
       <Router>
+        <TopBar/>
         <Routes/>
       </Router>
     </div>
