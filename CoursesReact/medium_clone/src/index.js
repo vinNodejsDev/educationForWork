d89f061460e9cc19b407338e4b28088db8adfe96
LtsProfessional <labmansoftac@gmail.com>
2020-09-02 19:20:00 +0300

    All

--- CoursesReact/medium_clone/src/index.js
+++ CoursesReact/medium_clone/src/index.js
@@ -5,15 +5,16 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'routes'
 import TopBar from 'components/topBar'
+import {CurrentUserProvider} from 'contexts/currentUser'
 
 const App = () => {
   return (
-    <div>
+    <CurrentUserProvider>
       <Router>
         <TopBar/>
         <Routes/>
       </Router>
-    </div>
+    </CurrentUserProvider>
   )
 }
 
